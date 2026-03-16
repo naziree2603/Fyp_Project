99b939ec9ef995429ba3122de49c096e9118b5b3
naziree2603 <naziree2603@gmail.com>
2026-03-16 19:02:03 +0800

    MainMenu

---
+++ Assets/Script/Camera/MenuManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+    //public GameObject howToPlayMenu;
+    public GameObject characterMenu;
+
+    public void OpenMainMenu()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+        //howToPlayMenu.SetActive(false);
+        characterMenu.SetActive(false);
+    }
+
+    public void OpenSettings()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+        //howToPlayMenu.SetActive(false);
+        characterMenu.SetActive(false);
+    }
+
+    //public void OpenHowToPlay()
+    //{
+        //mainMenu.SetActive(false);
+        //settingsMenu.SetActive(false);
+        //howToPlayMenu.SetActive(true);
+        //characterMenu.SetActive(false);
+    //}
+
+    public void OpenCharacterMenu()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+        //howToPlayMenu.SetActive(false);
+        characterMenu.SetActive(true);
+    }
+}
