d5f157097412bfa0ab03c98a46ba9333a7eb7fd7
naziree2603 <naziree2603@gmail.com>
2026-03-24 09:13:39 +0800

    Revert "EnemyHealth"
    This reverts commit a14e149e12dc325916814876110a2d2ed1ea274b.

--- Assets/Script/CharacterSpawner/CharacterSpawner.cs
+++ Assets/Script/CharacterSpawner/CharacterSpawner.cs
@@ -1,6 +1,5 @@
-﻿using UnityEngine;
+using UnityEngine;
 using Unity.Cinemachine;
-using UnityEngine.UI;
 
 public class CharacterSpawner : MonoBehaviour
 {
@@ -17,28 +16,15 @@ public class CharacterSpawner : MonoBehaviour
             transform.rotation
         );
 
-        // 🎥 Camera setup
+        // find the CameraFollow object inside the player
         Transform cameraTarget = player.transform.Find("CameraFollow");
 
+        // assign camera follow
         cineCamera.Follow = cameraTarget;
         cineCamera.LookAt = cameraTarget;
 
-        // 🎮 Movement setup
+        // assign camera to movement script
         PlayerMovement movement = player.GetComponent<PlayerMovement>();
         movement.SetCamera(Camera.main.transform);
-
-        // 🔥 NEW: Connect Slider to EnemyHealthBarManager
-        Slider slider = player.GetComponentInChildren<Slider>();
-
-        EnemyHealthBarManager manager = FindObjectOfType<EnemyHealthBarManager>();
-
-        if (manager != null && slider != null)
-        {
-            manager.SetSlider(slider);
-        }
-        else
-        {
-            Debug.LogWarning("Slider or EnemyHealthBarManager not found!");
-        }
     }
 }
